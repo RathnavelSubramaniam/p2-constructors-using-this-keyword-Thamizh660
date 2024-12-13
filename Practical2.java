32af14419bd13f887d0c514bcf88f74a1c0df652
THAMIZH ILAKIYA <thamizhelakiya.29@gmail.com>
2024-12-13 04:46:04 +0000

    program2

--- Practical2.java
+++ Practical2.java
@@ -11,7 +11,7 @@ class Product {
     {
         this.productId=productId;
         this.productName=productName;
-        this.price =price;
+        this.price=price;
         count++;
     }
     double discount(double Price,double Discount)
@@ -27,7 +27,7 @@ class Product {
 }
     public class Practical2
     {
-        public static void main(String[]args)
+        public static void main(String[] args)
         {
             Product product1=new Product(101,"Laptop",999.99);
             Product product2=new Product(202,"Smart Phone",499.95);
@@ -36,6 +36,7 @@ class Product {
             product1.displayProductInfo();
             product2.displayProductInfo();
             product3.displayProductInfo();
+            
             double DiscountedPrice=calcDiscount.discount(product1.price,10);
             System.out.println("Discounted Price : "+DiscountedPrice);
             System.out.println(Product.count);
